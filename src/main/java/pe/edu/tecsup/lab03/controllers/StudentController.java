5d3a1c6860a3ab32fa67bd55c8b468ccad635281
Adriana Chinchayhuara <adriana.chinchayhuar@tecsup.edu.pe>
2026-04-05 20:34:19 -0500

    Modificación de clases sprint-2 - Adriana Chinchayhuara

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,9 @@
 package pe.edu.tecsup.lab03.controllers;
 
+import pe.edu.tecsup.lab03.services.StudentService;
+
 public class StudentController {
+
+    StudentService service = new StudentService();
+
 }
--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -3,9 +3,23 @@ package pe.edu.tecsup.lab03.entities;
 public class StudentEntity {
     private String name;
     private int age;
+    private String career;
 
-    public StudentEntity(String name, int age) {
+    public StudentEntity(String name, int age, String career) {
         this.name = name;
         this.age = age;
+        this.career = career;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCareer() {
+        return career;
     }
 }
