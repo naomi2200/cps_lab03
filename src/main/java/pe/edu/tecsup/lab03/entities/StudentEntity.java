9f2f0f1a8895f566c0e5ba45d0286956ab992d97
Adriana Chinchayhuara <adriana.chinchayhuar@tecsup.edu.pe>
2026-04-05 19:33:09 -0500

    Añadir Entity - Adriana Chinchayhuara

---
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup.lab03.entities;
+
+public class StudentEntity {
+    private String name;
+    private int age;
+
+    public StudentEntity(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
