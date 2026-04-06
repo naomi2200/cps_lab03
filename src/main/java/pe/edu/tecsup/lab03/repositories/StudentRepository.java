d2516200034cb240e694fcb6f185d774b8daa68a
DevG28 <codepython22@gmail.com>
2026-04-05 20:29:54 -0500

    feat: update service and repository sprint-2

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,8 @@
 package pe.edu.tecsup.lab03.repositories;
 
 public class StudentRepository {
+
+    public void findAll() {
+        System.out.println("Sprint 2 - finding students in DB");
+    }
 }
--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -1,4 +1,8 @@
 package pe.edu.tecsup.lab03.services;
 
 public class StudentService {
+
+    public void getAllStudents() {
+        System.out.println("Sprint 2 - getting all students");
+    }
 }
